9127822327c75cfc0048f0e7d2ce92322240b220
Joshua Muheim <josh@josh.ch>
2020-05-13 12:54:03 +0200

    Add PPTX

--- song-book-builder.rb
+++ song-book-builder.rb
@@ -24,7 +24,9 @@ file = File.new("all-songs.md", "w")
 file.puts(markdown.join("\n"))
 file.close
 
-`pandoc -s -o all-songs.docx all-songs.md --no-highlight --toc --toc-depth=1 -V theme=night -V progress=false -V revealjs-url=./revealjs`
+`pandoc -s -o index.html all-songs.md --no-highlight --toc --toc-depth=1 -V theme=night -V progress=false -V revealjs-url=./revealjs`
+`pandoc -s -o songs.docx all-songs.md --no-highlight --toc --toc-depth=1`
+`pandoc -s -o songs.pptx all-songs.md --slide-level=2`
 
 result = File.open("index.html").read
 result.gsub!("<body>", '<body><input type="checkbox" id="toggle-chords-visibility" /><label for="toggle-chords-visibility"></label><a href="#" id="toggle-theme" onclick="document.getElementById(\'theme\').setAttribute(\'href\',\'revealjs/css/theme/serif.css\'); return false;"></a><a href="#/1" id="go-to-toc"></a>')
