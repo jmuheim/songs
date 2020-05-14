22477eccd8db74c12ceaa17d9ad784a7f035b66a
Joshua Muheim <josh@josh.ch>
2020-05-14 19:47:06 +0200

    Refactor

--- song-book-builder.rb
+++ song-book-builder.rb
@@ -10,50 +10,50 @@ markdown << <<~EOS
               ---
             EOS
 
-markdown << File.open("Introduction.md").read
+markdown << File.open("content/Introduction.md").read
 
-markdown += Dir["songs/B*.md"].sort.map do |filename|
+markdown += Dir["content/songs/*.md"].sort.map do |filename|
               content = File.open(filename).read
-            
+
               content.gsub!(/(\[)(([A-Z]).*?)(\])([^\(])/) do
                 "`#{$2}`{.#{$3.downcase}}#{$5}"
               end
             end
 
-file = File.new("tmp/all-songs.md", "w")
+file = File.new("all-songs.md", "w")
 file.puts(markdown.join("\n"))
 file.close
 
 # HTML slides (Reveal.js)
-puts `pandoc -t revealjs -s -o index.html tmp/all-songs.md --slide-level=2 --no-highlight --toc --toc-depth=1 -V theme=night -V progress=false -V revealjs-url=./revealjs`
+puts `pandoc -t revealjs -s -o index.html all-songs.md --slide-level=2 --no-highlight --toc --toc-depth=1 -V theme=night -V progress=false -V revealjs-url=./style/revealjs`
 
 result = File.open("index.html").read
 
-result.gsub!("<body>", '<body><input type="checkbox" id="toggle-chords-visibility" /><label for="toggle-chords-visibility"></label><a href="#" id="toggle-theme" onclick="document.getElementById(\'theme\').setAttribute(\'href\',\'revealjs/css/theme/serif.css\'); return false;"></a><a href="#/1" id="go-to-toc"></a>')
-result.gsub!('<style>', '<style>' + File.open("css/night.css").read + File.open("css/shared.css").read)
+result.gsub!("<body>", '<body><input type="checkbox" id="toggle-chords-visibility" /><label for="toggle-chords-visibility"></label><a href="#" id="toggle-theme" onclick="document.getElementById(\'theme\').setAttribute(\'href\',\'revealjs/style/theme/serif.css\'); return false;"></a><a href="#/1" id="go-to-toc"></a>')
+result.gsub!('<style>', '<style>' + File.open("style/night.css").read + File.open("style/shared.css").read)
 
 file = File.open("index.html", "w")
 file.write(result)
 file.close
 
 # Word
-# puts `pandoc -s -o songs.docx tmp/all-songs.md --no-highlight --toc --toc-depth=1`
+# puts `pandoc -s -o songs.docx all-songs.md --no-highlight --toc --toc-depth=1`
 
 # Powerpoint
-# puts `pandoc -s -o songs.pptx tmp/all-songs.md --slide-level=2`
+# puts `pandoc -s -o songs.pptx all-songs.md --slide-level=2`
 
 # Printable song book
-puts `pandoc -t revealjs -s -o tmp/print.html tmp/all-songs.md --slide-level=2 --no-highlight --toc --toc-depth=1 -V theme=serif -V progress=false -V revealjs-url=../revealjs`
+puts `pandoc -t revealjs -s -o print.html all-songs.md --slide-level=2 --no-highlight --toc --toc-depth=1 -V theme=serif -V progress=false -V revealjs-url=./style/revealjs`
 
-result = File.open("tmp/print.html").read
+result = File.open("print.html").read
 
-result.gsub!('<style>', '<style>' + File.open("css/serif.css").read + File.open("css/shared.css").read) # Add 
-result.gsub!(/<section id="resources-?[\d+]?" class="slide level2">(.*?)<\/section>/m, "") # Remove "Resources" slides (they contain links that are useless/ugly in a printed document)
+result.gsub!('<style>', '<style>' + File.open("style/serif.css").read + File.open("style/shared.css").read) # Add 
+result.gsub!(/<section id="resources[-\d]*?" class="slide level2">(.*?)<\/section>/m, "") # Remove "Resources" slides (they contain links that are useless/ugly in a printed document)
+result.gsub!('<section id="title-slide"', '<section id="title-slide" data-background-image="style/background.jpg"') # Add background to title slide
 
-file = File.open("tmp/print.html", "w")
+file = File.open("print.html", "w")
 file.write(result)
 file.close
 
-print_file = "#{URI::encode(File.expand_path(File.dirname(__FILE__))).to_s}/tmp/print.html"
-`rm -rf tmp/screenshots/*`
-puts `decktape --screenshots --screenshots-directory=tmp/screenshots --screenshots-size=2400x1800 --screenshots-format=jpg file://#{print_file} print.pdf`
+print_file = "#{URI::encode(File.expand_path(File.dirname(__FILE__))).to_s}/print.html"
+puts `decktape -s 1600x1200 file://#{print_file} print.pdf`
